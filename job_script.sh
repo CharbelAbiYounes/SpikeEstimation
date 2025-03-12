ebef1319f305c49a23751fadfc45bf7a794a4c2b
CharbelAbiYounes <cyounes@uw.edu>
2025-03-12 08:00:09 -0700

    .

--- job_script.sh
+++ job_script.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=compute     # Partition to use
 #SBATCH --nodes=4               # Number of nodes
 #SBATCH --ntasks=4              # One task per node
-#SBATCH --cpus-per-task=8      # 40 CPUs per node
+#SBATCH --cpus-per-task=2      # 40 CPUs per node
 #SBATCH --mem=175G              # Memory allocation
 #SBATCH --time=48:00:00         # Maximum runtime
 #SBATCH --output=cyounes-job-%j.out  # Standard output log
@@ -17,7 +17,7 @@ scontrol show hostnames > hosts.txt
 cat hosts.txt  
 
 # Set the number of Julia threads and assign CPUs to the task
-export JULIA_NUM_THREADS=40
+export JULIA_NUM_THREADS=8
 
 # Run the Julia script with taskset to bind the process to specific CPUs
-srun taskset -c 0-39 nice -n 0 julia Example1.jl > output.log 2>&1
+srun taskset -c 0-7 nice -n 0 julia Example1.jl > output.log 2>&1
